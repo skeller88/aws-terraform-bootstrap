6342302f22b72601dc8fe8ab22f1b16618fa7fb3
skeller88 <skeller88@gmail.com>
2018-03-06 21:25:15 -0800

    Remove vpc endpoints as they appear to be unnecessary.

--- terraform/hello_world_vpc.tf
+++ terraform/hello_world_vpc.tf
@@ -72,23 +72,6 @@ resource "aws_subnet" "hello_world_public_west_1a" {
   vpc_id = "${aws_vpc.hello_world.id}"
 }
 
-# VPC endpoints
-# Be careful as "Gateway" and "Interface" types require different configurations
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = "${aws_vpc.hello_world.id}"
-  service_name = "com.amazonaws.us-west-1.s3"
-  route_table_ids = ["${aws_route_table.hello_world_private_west_1a_subnet_route_table.id}",
-    "${aws_route_table.hello_world_private_west_1b_subnet_route_table.id}"]
-}
-
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id       = "${aws_vpc.hello_world.id}"
-  service_name = "com.amazonaws.us-west-1.ssm"
-  vpc_endpoint_type = "Interface"
-  security_group_ids = ["${aws_security_group.hello_world_lambda_vpc_security_group.id}"]
-  subnet_ids = ["${aws_subnet.hello_world_private_west_1a.id}", "${aws_subnet.hello_world_private_west_1b.id}"]
-}
-
 output "default_vpc_id" {
   value = "${aws_vpc.hello_world.id}"
 }
